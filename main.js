3ef18630abeb6385f1db3a111830f67208884ea0
airrs1 <101538856+airrs1@users.noreply.github.com>
2022-12-16 21:06:07 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+var first_name="Matt";
+var last_name="Sims";
+var full_name= first_name.concat(last_name);
+console.log(full_name);
+
+var a=250;
+var b=16;
+var plus= a + b;
+console.log(plus);
+
+var times= a * b;
+console.log(times);
+
+var minus= a - b;
+console.log(minus);
+
+var divi= a / b;
+console.log(divi);
